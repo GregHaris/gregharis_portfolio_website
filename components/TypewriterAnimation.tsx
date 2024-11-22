e9c2e2af972e8a3e7e7cf93c788e969611002bf6
Grëg Häris <d.gregharis@gmail.com>
2024-11-22 20:35:42 +0100

    fix: remove full stack web developer from animation

--- components/TypewriterAnimation.tsx
+++ components/TypewriterAnimation.tsx
@@ -5,7 +5,6 @@ import { TypeAnimation } from 'react-type-animation';
 export function TypewriterAnimation() {
   return <TypeAnimation sequence={[
     'Software Developer', 2000,
-    'Full Stack Web Developer', 2000,
     'Problem Solver', 2000,
   ]}
     wrapper='span'
