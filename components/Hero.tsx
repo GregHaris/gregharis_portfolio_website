1b3718d46debe444d73348fff733afbe874a68a3
Grëg Häris <d.gregharis@gmail.com>
2024-11-22 11:47:03 +0100

    style: add animation
    add animation to display skills

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,12 +3,13 @@
 import { ArrowDownIcon, FileText } from 'lucide-react';
 import { SiGithub, SiLinkedin } from '@icons-pack/react-simple-icons';
 import Link from 'next/link';
+import { TypewriterAnimation } from './TypewriterAnimation';
 
 export default function Hero() {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground px-4 pt-16">
-      <h1 className="text-8xl font-bold mb-4 text-center">Grëg Häris</h1>
-      <h2 className="text-3xl mb-8 text-center">Software Developer</h2>
+      <h1 className="text-8xl font-bold mb-6 text-center">Grëg Häris</h1>
+      <h2 className="text-3xl mb-8 text-center"> <TypewriterAnimation/></h2>
       <p className="text-xl mb-12 max-w-2xl text-center">
         Crafting elegant solutions to complex problems
       </p>
--- components/TypewriterAnimation.tsx
+++ components/TypewriterAnimation.tsx
@@ -3,5 +3,13 @@
 import { TypeAnimation } from 'react-type-animation';
 
 export function TypewriterAnimation() {
-  return <TypeAnimation sequence={['Grëg Häris', 2000]} speed={50} />;
+  return <TypeAnimation sequence={[
+    'Software Developer', 2000,
+    'Full Stack Web Developer', 2000,
+    'Problem Solver', 2000,
+  ]}
+    wrapper='span'
+    speed={50}
+    repeat={Infinity}
+  />;
 }
