bedbb99cf69b17e8ab04b76007284ca34271a26f
Grëg Häris <d.gregharis@gmail.com>
2024-11-22 11:18:49 +0100

    add typeWriterAnimation component
    To animate texts

---
+++ components/TypewriterAnimation.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { TypeAnimation } from 'react-type-animation';
+
+export function TypewriterAnimation() {
+  return <TypeAnimation sequence={['Grëg Häris', 2000]} speed={50} />;
+}
