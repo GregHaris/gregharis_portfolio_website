afbae26212830a4ae290e30c09742ae3cbbcb86e
Grëg Häris <d.gregharis@gmail.com>
2024-11-25 21:49:40 +0100

    style: add border to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 text-black dark:bg-black py-4">
-      <div className="container mx-auto text-center">
-        <p className="text-black dark:text-white text-sm">
+    <footer className="bg-gray-100 text-black dark:bg-black border-t border-gray-200 dark:border-gray-800">
+      <div className="container mx-auto py-4 px-4">
+        <p className="text-black dark:text-white text-sm text-center">
           Coded with <span className="text-red-500">❤️</span> by Grëg Häris
         </p>
       </div>
