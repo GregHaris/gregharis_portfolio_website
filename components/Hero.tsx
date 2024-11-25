5883761588ee02b68538f3e8494f178697fd612b
Grëg Häris <d.gregharis@gmail.com>
2024-11-25 21:01:23 +0100

    style: set the text color of the h2 to gray-500

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground px-4 pt-16">
       <h1 className="text-8xl font-bold mb-6 text-center">Grëg Häris</h1>
-      <h2 className="text-3xl mb-8 text-center">
+      <h2 className="text-3xl mb-8 text-center text-gray-500">
         {' '}
         <TypewriterAnimation />
       </h2>
