b1994a1ec0ab74886b382a37fba822ec70bc3c26
Grëg Häris <d.gregharis@gmail.com>
2024-11-25 21:57:17 +0100

    feat: add Grëg Häris © 2024

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,11 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 text-black dark:bg-black border-t border-gray-200 dark:border-gray-800">
       <div className="container mx-auto py-4 px-4">
-        <p className="text-black dark:text-white text-sm text-center">
+        <p className="text-black dark:text-white text-sm text-center mb-4">
           Coded with <span className="text-red-500">❤️</span> by Grëg Häris
+        </p>
+        <p className="text-black dark:text-white text-sm text-center">
+          Grëg Häris © 2024
         </p>
       </div>
     </footer>
