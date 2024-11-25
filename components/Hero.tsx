037ce64eb834b6ffc292f8f8191f5d2fa5b4d646
Grëg Häris <d.gregharis@gmail.com>
2024-11-25 20:07:34 +0100

    feat: add x.com to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import { ArrowDownIcon, FileText } from 'lucide-react';
-import { SiGithub, SiLinkedin } from '@icons-pack/react-simple-icons';
+import {
+  SiGithub,
+  SiLinkedin,
+  SiX,
+  SiGmail,
+} from '@icons-pack/react-simple-icons';
 import Link from 'next/link';
 import { TypewriterAnimation } from './TypewriterAnimation';
 
@@ -9,7 +14,10 @@ export default function Hero() {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground px-4 pt-16">
       <h1 className="text-8xl font-bold mb-6 text-center">Grëg Häris</h1>
-      <h2 className="text-3xl mb-8 text-center"> <TypewriterAnimation/></h2>
+      <h2 className="text-3xl mb-8 text-center">
+        {' '}
+        <TypewriterAnimation />
+      </h2>
       <div className="flex space-x-4 mb-12">
         <Link
           href="https://www.linkedin.com/in/dgregharis"
@@ -30,7 +38,15 @@ export default function Hero() {
           <span className="sr-only">GitHub</span>
         </Link>
         <Link
-          href="/john-doe-cv.pdf"
+          href="https://www.x.com/IamAbovExcuse"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center gap-2 text-foreground hover:text-primary transition-colors"
+        >
+          <SiX className="w-5 h-5" />
+        </Link>
+        <Link
+          href="/gregharisCV.pdf"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-gray-600 dark:hover:text-gray-300 transition-colors"
