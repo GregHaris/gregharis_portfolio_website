a42a3dae0cf10bd437e11bc2c29c6c5020263801
Grëg Häris <d.gregharis@gmail.com>
2024-11-25 21:39:29 +0100

    feat: add footer component to display footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-black dark:bg-black py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-black dark:text-white text-sm">
+          Coded with <span className="text-red-500">❤️</span> by Grëg Häris
+        </p>
+      </div>
+    </footer>
+  );
+}
