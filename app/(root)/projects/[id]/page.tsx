1ae7bdc4516063f08434b8771af11b303936209d
Grëg Häris <d.gregharis@gmail.com>
2025-04-06 17:45:57 +0100

    chore: move files into /(root)/

---
+++ app/(root)/projects/[id]/page.tsx
@@ -0,0 +1,66 @@
+import Link from 'next/link';
+import { ArrowLeft } from 'lucide-react';
+import { notFound } from 'next/navigation';
+
+import ProjectImage from '@/components/ProjectImage';
+import ProjectLinks from '@/components/ProjectLinks';
+import { Project } from '@/types/Project';
+
+import gregChatbot from '@/projects/gregChatbot';
+
+// Array of projects
+const projects: Project[] = [gregChatbot];
+
+export default async function ProjectPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  // Await the params object to resolve it
+  const { id } = await params;
+
+  // Find the project by ID
+  const project = projects.find((p) => p.id === id);
+
+  // If no project is found, return a 404 page
+  if (!project) {
+    notFound();
+  }
+
+  return (
+    <div className="min-h-screen bg-background text-foreground pt-20">
+      <div className="max-w-4xl mx-auto px-4 py-8">
+        <Link
+          href="/#projects"
+          className="inline-flex items-center text-black dark:text-slate-400 hover:underline mb-6"
+        >
+          <ArrowLeft className="mr-2" /> Back to all projects
+        </Link>
+        <h1 className="text-4xl font-bold mb-4">{project.title}</h1>
+        {/* First Image: Use object-fit: cover */}
+        <ProjectImage
+          src={project.image1}
+          alt={project.title}
+          objectFit="cover"
+        />
+        <div className="space-y-6 pb-4">{project.fullDescription}</div>
+        {/* Second Image: Use object-fit: contain */}
+        <ProjectImage
+          src={project.image2}
+          alt={project.title}
+          objectFit="contain"
+        />
+        <h2 className="text-2xl font-semibold mb-4">Technologies Used</h2>
+        <ul className="list-disc list-inside mb-6">
+          {project.technologies.map((tech, index) => (
+            <li key={index}>{tech}</li>
+          ))}
+        </ul>
+        <ProjectLinks
+          githubLink={project.githubLink}
+          liveLink={project.liveLink}
+        />
+      </div>
+    </div>
+  );
+}
